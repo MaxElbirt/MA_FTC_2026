302ebbf3b07d9bb632990147a86813c6a07aecb8
max-elbirt <117280047+max-elbirt@users.noreply.github.com>
2026-02-04 15:43:08 +0200

    Constants added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+
+    //** Hardware Names **//
+
+    public static final double WHEEL_DIAMETER_ME = 80;
+    public static final double TICKS_NUM = 333;
+    public static final double TICKS_PER_ME = TICKS_NUM / WHEEL_DIAMETER_ME;
+
+    //Names
+    public static final String LEFT_DRIVE_NAME = "leftMotor";
+    public static final String RIGHT_DRIVE_NAME = "rightMotor";
+    public static final String SHOOTER_NAME = "shooterMotor";
+    public static final String KICKER_NAME = "kickerMotor";
+
+    //Speeds
+    public static final int DRIVE_SPEED = 100;
+    public static final int TURN_SPEED = 50;
+    public static final int SHOOTER_SPEED_FAR = 100;
+    public static final int SHOOTER_SPEED_NEAR = 50;
+    public static final int KICKER_SPEED = 100;
+
+
+}
