88d0688e0d955f57f3fad7c702c14f10820c55ca
unknown <tdg2108@gmail.com>
2026-02-23 22:06:49 +0200

    new auto position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTutorial.java
@@ -23,13 +23,18 @@ public class AutonomousTutorial extends LinearOpMode {
 
 
 //            how to write the autonomous:
-
+        // V = 13.50 - 13.70- 13.30
         long backwards = 800;
         short shoot = 3000;
         short getreadyshoter = 3000;
         short turnoff2 = 0;
         short kicker = 600;
         short wait = 2000;
+        short moveright = 770;
+        short turnback = 775;
+        short moveback = 2000;
+        short movefowrored = 300;
+
 
         marathonMap.hood.setPosition(0.45);
         marathonMap.frontLeftMotor.setPower(-0.6);
@@ -45,7 +50,7 @@ public class AutonomousTutorial extends LinearOpMode {
         marathonMap.backRightMotor.setPower(0);
         sleep(getreadyshoter);
 
-        marathonMap.shooterMotor1.setVelocity(1400);
+        marathonMap.shooterMotor1.setVelocity(1150);
         marathonMap.shooterMotor2.setPower(marathonMap.shooterMotor1.getPower());
         sleep(4000);
         marathonMap.kickerMotor.setPower(-0.6);
@@ -70,8 +75,133 @@ public class AutonomousTutorial extends LinearOpMode {
 
 
 
+        marathonMap.intakeMotor.setPower(-0.8);
+        marathonMap.frontLeftMotor.setPower(-0.7);
+        marathonMap.frontRightMotor.setPower(-0.7);
+        marathonMap.backLeftMotor.setPower(-0.7);
+        marathonMap.backRightMotor.setPower(-0.7);
+        sleep(movefowrored);
+
+        marathonMap.frontLeftMotor.setPower(0.7);
+        marathonMap.frontRightMotor.setPower(-0.7);
+        marathonMap.backLeftMotor.setPower(0.7);
+        marathonMap.backRightMotor.setPower(-0.7);
+       sleep(turnback);
+
+        marathonMap.frontLeftMotor.setPower(-0.8);
+        marathonMap.backRightMotor.setPower(-0.8);
+        marathonMap.frontRightMotor.setPower(0.8);
+        marathonMap.backLeftMotor.setPower(0.8);
+
+       sleep(moveright);
+        marathonMap.frontLeftMotor.setPower(-0.4);
+        marathonMap.frontRightMotor.setPower(-0.4);
+        marathonMap.backLeftMotor.setPower(-0.4);
+        marathonMap.backRightMotor.setPower(-0.4);
+       sleep(moveback);
 
 
+
+        marathonMap.kickerMotor.setPower(-0.5);
+        sleep(400);
+        marathonMap.kickerMotor.setPower(0);
+        sleep(300);
+
+        marathonMap.kickerMotor.setPower(-0.5);
+        sleep(400);
+        marathonMap.kickerMotor.setPower(0);
+        sleep(300);
+
+        marathonMap.kickerMotor.setPower(-0.5);
+        sleep(400);
+        marathonMap.kickerMotor.setPower(0);
+        sleep(300);
+
+        marathonMap.frontLeftMotor.setPower(0.7);
+        marathonMap.frontRightMotor.setPower(0.7);
+        marathonMap.backLeftMotor.setPower(0.7);
+        marathonMap.backRightMotor.setPower(0.7);
+        sleep(800);
+
+        marathonMap.frontLeftMotor.setPower(0.7);
+        marathonMap.frontRightMotor.setPower(-0.7);
+        marathonMap.backLeftMotor.setPower(-0.7);
+        marathonMap.backRightMotor.setPower(0.7);
+        sleep(1700);
+
+        marathonMap.frontLeftMotor.setPower(0.7);
+        marathonMap.frontRightMotor.setPower(-0.7);
+        marathonMap.backLeftMotor.setPower(0.7);
+        marathonMap.backRightMotor.setPower(-0.7);
+        sleep(turnback);
+
+
+        marathonMap.shooterMotor1.setVelocity(1300);
+        marathonMap.shooterMotor2.setPower(marathonMap.shooterMotor1.getPower());
+        marathonMap.kickerMotor.setPower(-0.6);
+        sleep(kicker);
+
+        marathonMap.kickerMotor.setPower(0);
+        sleep(wait);
+
+        marathonMap.kickerMotor.setPower(-0.6);
+        sleep(kicker);
+
+        marathonMap.kickerMotor.setPower(0);
+        sleep(wait);
+
+        marathonMap.kickerMotor.setPower(-0.6);
+        sleep(kicker);
+
+//  stop move forward
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+
+// stop turnback
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+
+// stop moveright
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+
+// stop moveback
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+ // stop intake
+        marathonMap.kickerMotor.setPower(0);
+
+// stop  sleep(800);
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+
+//  stop sleep(1700);
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+
+//  stop sleep(turnback);
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+
+
+ // stop shoter
+
+        marathonMap.shooterMotor1.setVelocity(0);
+        marathonMap.shooterMotor2.setPower(marathonMap.shooterMotor1.getPower());
 
     }
 }
