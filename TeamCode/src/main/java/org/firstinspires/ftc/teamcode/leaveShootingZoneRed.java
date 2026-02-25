2377c8b9e8e4e8a5215cb6e00c255574dc020fd4
itay10001 <157873702+itay10001@users.noreply.github.com>
2026-02-25 10:51:29 +0200

    added the autonomous that leaves the shooting zone for points in the red side

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaveShootingZoneRed.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Autonomous
+public class leaveShootingZoneRed extends LinearOpMode {
+    private static final Logger log = LoggerFactory.getLogger(leaveShootingZoneRed.class);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        RobotHardwareMap marathonMap = new RobotHardwareMap();
+        marathonMap.init(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+
+        long backwards1 = 600;
+        long right = 800;
+        marathonMap.frontRightMotor.setPower(-1);
+        marathonMap.frontLeftMotor.setPower(-1);
+        marathonMap.backRightMotor.setPower(-1);
+        marathonMap.backLeftMotor.setPower(-1);
+        sleep(right);
+        marathonMap.frontLeftMotor.setPower(1);
+        marathonMap.backRightMotor.setPower(1);
+        marathonMap.frontRightMotor.setPower(-1);
+        marathonMap.backLeftMotor.setPower(-1);
+        sleep(backwards1);
+        marathonMap.frontLeftMotor.setPower(0);
+        marathonMap.backRightMotor.setPower(0);
+        marathonMap.frontRightMotor.setPower(0);
+        marathonMap.backLeftMotor.setPower(0);
+
+
+
+
+    }
+}
+
+
