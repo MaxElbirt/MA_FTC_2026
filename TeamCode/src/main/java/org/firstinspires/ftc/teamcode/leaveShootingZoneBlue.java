197d635cb0ff2e2d8d9c1d45ee1a15e8b42d2a86
itay10001 <157873702+itay10001@users.noreply.github.com>
2026-02-25 10:55:26 +0200

    something didnt work, so now i am commiting the leaving the shooting zone for blue again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaveShootingZoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaveShootingZoneBlue.java
@@ -2,12 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Autonomous
-public class leaveShootingZoneRed extends LinearOpMode {
-    private static final Logger log = LoggerFactory.getLogger(leaveShootingZoneRed.class);
+public class leaveShootingZoneBlue extends LinearOpMode {
+    private static final Logger log = LoggerFactory.getLogger(leaveShootingZoneBlue.class);
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -28,10 +29,10 @@ public class leaveShootingZoneRed extends LinearOpMode {
         marathonMap.backRightMotor.setPower(-1);
         marathonMap.backLeftMotor.setPower(-1);
         sleep(right);
-        marathonMap.frontLeftMotor.setPower(1);
-        marathonMap.backRightMotor.setPower(1);
-        marathonMap.frontRightMotor.setPower(-1);
-        marathonMap.backLeftMotor.setPower(-1);
+        marathonMap.frontLeftMotor.setPower(-1);
+        marathonMap.backRightMotor.setPower(-1);
+        marathonMap.frontRightMotor.setPower(1);
+        marathonMap.backLeftMotor.setPower(1);
         sleep(backwards1);
         marathonMap.frontLeftMotor.setPower(0);
         marathonMap.backRightMotor.setPower(0);
