13afb0d5bbe153421fa12eed84da25a3ed2ed840
oritzgur-dot <oritzgur@gmail.com>
2026-03-12 15:49:45 +0200

    idk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomusByTicksPractice.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous
+public class AutonomusByTicksPractice extends LinearOpMode {
+
+    DcMotorEx frontLeftMotor;
+    DcMotorEx frontRightMotor;
+    DcMotorEx backLeftMotor;
+    DcMotorEx backRightMotor;
+
+    @Override
+    public void runOpMode() {
+
+        // Hardware mapping
+        frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotorEx.class, "backRightMotor");
+
+        // Reverse right side (typical drivetrain)
+        frontRightMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
+        // Brake so robot stops cleanly
+        frontLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        resetEncoders();
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+
+        moveTicks(1000, 0.5);
+    }
+
+    public void resetEncoders(){
+
+        frontLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void moveTicks(int ticks, double power){
+
+        int flTarget = frontLeftMotor.getCurrentPosition() + ticks;
+        int frTarget = frontRightMotor.getCurrentPosition() + ticks;
+        int blTarget = backLeftMotor.getCurrentPosition() + ticks;
+        int brTarget = backRightMotor.getCurrentPosition() + ticks;
+
+        frontLeftMotor.setTargetPosition(flTarget);
+        frontRightMotor.setTargetPosition(frTarget);
+        backLeftMotor.setTargetPosition(blTarget);
+        backRightMotor.setTargetPosition(brTarget);
+
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+//        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(power);
+//        backLeftMotor.setPower(power);
+        backRightMotor.setPower(power);
+
+        // Debug loop showing encoder ticks
+        while(opModeIsActive() &&
+                (frontLeftMotor.isBusy() ||
+                        frontRightMotor.isBusy() ||
+                        backLeftMotor.isBusy() ||
+                        backRightMotor.isBusy())) {
+
+            telemetry.addData("FL ticks", frontLeftMotor.getCurrentPosition());
+            telemetry.addData("FR ticks", frontRightMotor.getCurrentPosition());
+            telemetry.addData("BL ticks", backLeftMotor.getCurrentPosition());
+            telemetry.addData("BR ticks", backRightMotor.getCurrentPosition());
+            telemetry.update();
+        }
+
+
+
+    }
+
+
+    }
